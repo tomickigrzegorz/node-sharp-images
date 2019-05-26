dde3a3e32f2d4b1ab4f4555344345fd9f7e3fcfb
Grzegorz Tomicki <gtomicki@gmail.com>
2019-05-26 13:16:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+## Photo converter
+
+### Instruction
+Instal latest Node.js
+`git clone `
+Open `cd shareimage`  
+Next run `yarn` or `npm install`
+
+### How to run
+
+First you must have folder with images, default is  `sources` folder wher is all images to convert.
+Default folder to output images files it is `public`.
+Exampel `node script.js --breakpoints=576`
+
+### Configure app
+
+arguments | default | require | description
+---- | :-------: | :--------: | -----------
+**--breakpoints** |  | ✔ | --breakpoints=500,600,700 or --breakpoints=500
+**--format** | `jpg` |  | --format=webp or --format=png
+**--sources** | `sources` | | --sources=images or --sources=images/berlin
+**--output** | `output` | | --keep=picture or --sources=picure/berlin
+**--keep** |  | | --keep=true Keep output folder
+
+### Examples
+
+Generate one size - 576px width
+```
+node script.js --breakpoints=576
+```
+This command generate `output` folder. Output folder cointains folder `576` with generated images.
+Now we need webp format, but not removeing images `jpg`.
+```
+node script.js --breakpoints=576 --format=webp --keep=true
+```
+
+> another example of use
+
+```
+node script.js --breakpoints=576,768 --sources=sources/berlin --output=images/berlin
+```
+This command will create such a structure for us:
+```
+images
+├──berlin
+├──├──576
+├──├──├──IMG_001.jpg
+├──├──├──IMG_002.jpg
+├──├──├──...
+├──├──768
+├──├──├──IMG_001.jpg
+├──├──├──IMG_002.jpg
+└──├──├──...
+```
+
+```
+structure
+├──berlin
+├──├──IMG_001.jpg
+├──├──IMG_002.jpg
+└──├──...
+```
+
+## Other information
+
+The images used are from 
+[https://images.unsplash.com](https://images.unsplash.com)
---
+++ script.js
@@ -0,0 +1,94 @@
+const fs = require('fs')
+const { promisify } = require('util')
+const args = require('minimist')(process.argv.slice(2))
+const mkdirp = require('mkdirp')
+const sharp = require('sharp')
+const rimraf = require('rimraf')
+
+const readDirAsync = promisify(fs.readdir)
+const mkDirpAsync = promisify(mkdirp)
+
+const outpuDir = (output, keepDir) => {
+  if (keepDir) return
+
+  if (fs.existsSync(output)) {
+    rimraf.sync(output)
+  }
+  mkDirpAsync(output)
+}
+
+const setTypeImage = (imageSharp, outputFormat) => {
+  switch (outputFormat) {
+    case 'jpg': return imageSharp.jpeg({
+      quality: 75,
+      progressive: true
+    })
+    case 'webp': return imageSharp.webp({
+      quality: 60
+    })
+    case 'png': return imageSharp.png({
+      compressionLevel: 6,
+      quality: 65,
+      progressive: true
+    })
+    default:
+      break
+  }
+}
+
+// [576,768,992,1200]
+const converJPGtoWebP = async () => {
+  try {
+    const { breakpoints, output, sources, format, height, keep } = args
+    const keepDir = (keep === undefined || keep === null) ? '' : keep.toLowerCase() === 'true'
+
+    const points = breakpoints
+    const outputArg = output || 'public'
+    const input = sources || 'sources'
+    const outputFormat = format || 'jpg'
+    const outputHeight = parseInt(height) || 800
+    const breakpointsArgs = Number.isInteger(points) === true ? Array.of(points) : points.split(',')
+
+    // remove output folder and create new one
+    outpuDir(outputArg, keepDir)
+
+    // get async files
+    const res = await readDirAsync(input)
+
+    breakpointsArgs.forEach(breakpointsFolder => {
+      // remove output folder and create new one
+      outpuDir(`${outputArg}/${breakpointsFolder}`, keepDir)
+
+      res.forEach(async (image) => {
+        // file extension replace
+        const newImage = image.replace(/\.[^\\.]+$/, `.${outputFormat}`)
+
+        const imageSharp = await sharp(`${input}/${image}`)
+        imageSharp.resize({
+          // set width from command line or get default with 1200px
+          // --breakpoints=576 - required parameter
+          width: parseInt(breakpointsFolder),
+          // default max height is 800px
+          // if you set height then width i change also
+          height: outputHeight,
+          // maintain aspect ratio
+          fit: sharp.fit.inside,
+          // do not enlarge if the width or height are already less than the specified dimensions
+          withoutEnlargement: true
+
+        })
+        // set type image [jepg, png, webp]
+        // --format=webp or default jpg
+        setTypeImage(imageSharp, outputFormat)
+        // save to specific folder
+        // --breakpoints=576 - required parameter
+        imageSharp.toFile(`${outputArg}/${breakpointsFolder}/${newImage}`)
+      })
+    })
+  } catch (err) {
+    // return Object.assign(err)
+    console.log(err.message)
+  }
+}
+
+converJPGtoWebP()
