fb0d7bea246d8e74b035ef99a1ceb8ec1a016f22
Grzegorz Tomicki <gtomik@gmail.com>
2019-11-13 17:04:49 +0100

    Adding interactive selection

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const fs = require('fs')
+const { existsSync, readdir, mkdirSync } = require('fs')
 const { promisify } = require('util')
-const args = require('minimist')(process.argv.slice(2))
-const mkdirp = require('mkdirp')
+const { remove } = require('fs-extra')
+const path = require('path')
 const sharp = require('sharp')
-const rimraf = require('rimraf')
-
-const readDirAsync = promisify(fs.readdir)
-const mkDirpAsync = promisify(mkdirp)
+const inquirer = require('inquirer')
+const readDirAsync = promisify(readdir)
 
 const outpuDir = (output, keepDir) => {
-  if (keepDir) return
-
-  if (fs.existsSync(output)) {
-    rimraf.sync(output)
+  try {
+    if (!keepDir) {
+      remove('./output')
+        .then(() => {
+          mkdirSync(output, { recursive: true })
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    } else {
+      mkdirSync(output, { recursive: true })
+    }
+  } catch (err) {
+    console.error(err)
   }
-  mkDirpAsync(output)
 }
 
 const setImageType = (imageSharp, outputFormat) => {
@@ -37,36 +44,42 @@ const setImageType = (imageSharp, outputFormat) => {
 }
 
 // [576,768,992,1200]
-const converter = async () => {
+const converter = async (args) => {
   try {
-    const { b, o, s, f, height, k } = args
-    const keepDir = (k === undefined || k === null) ? '' : k.toLowerCase() === 'true'
+    const { points, input, format, height, keepDir } = args
 
-    const points = b // breakpoints
-    const outputArg = o || 'output' // output
-    const input = s || 'sources' // sources
-    const outputFormat = f || 'jpg' // format
-    const outputHeight = parseInt(height) || 800
+    const outputFormat = format
+    const outputHeight = height
     const breakpointsArgs = Number.isInteger(points) === true ? Array.of(points) : points.split(',')
 
+    console.log(path.resolve())
+
+    const outputDir = path.resolve(`./output/${input}`)
+    const sourceDir = path.resolve(`./sources/${input}`)
+
+    // checking if the folder is in sources
+    if (!existsSync(sourceDir)) {
+      console.log(`The specified folder does not exist - ${sourceDir}`)
+      return false
+    }
+
     // remove output folder and create new one
-    outpuDir(outputArg, keepDir)
+    outpuDir(outputDir, keepDir)
 
     // get async files
-    const res = await readDirAsync(input)
+    const res = await readDirAsync(sourceDir)
 
     breakpointsArgs.forEach(breakpointsFolder => {
       // remove output folder and create new one
-      outpuDir(`${outputArg}/${breakpointsFolder}`, keepDir)
+      outpuDir(`${outputDir}/${breakpointsFolder}`, keepDir)
 
       res.forEach(async (image) => {
         // file extension replace
         const newImage = image.replace(/\.[^\\.]+$/, `.${outputFormat}`)
 
-        const imageSharp = await sharp(`${input}/${image}`)
+        const imageSharp = await sharp(`${sourceDir}/${image}`)
         imageSharp.resize({
           // set width from command line or get default with 1200px
-          // --breakpoints=576 - required parameter
           width: parseInt(breakpointsFolder),
           // default max height is 800px
           // if you set height then width i change also
@@ -77,12 +90,10 @@ const converter = async () => {
           withoutEnlargement: true
 
         })
-        // set type image [jepg, png, webp]
-        // --format=webp or default jpg
+        // set type image [jpg, png, webp]
         setImageType(imageSharp, outputFormat)
-        // save to specific folder
-        // --breakpoints=576 - required parameter
-        imageSharp.toFile(`${outputArg}/${breakpointsFolder}/${newImage}`)
+        // save to specific folder - breakpoints
+        imageSharp.toFile(`${outputDir}/${breakpointsFolder}/${newImage}`)
       })
     })
   } catch (err) {
@@ -91,4 +102,49 @@ const converter = async () => {
   }
 }
 
-converter()
+// converter()
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'points',
+    message: 'Select breakpoints e.g. 576,768,992,1200',
+    default: '576'
+  },
+  {
+    type: 'input',
+    name: 'input',
+    message: 'Folder to convert',
+    validate: input => {
+      if (!input.length) {
+        return 'Enter the folder to convert'
+      }
+      return true
+    }
+  },
+  {
+    type: 'list',
+    name: 'format',
+    choices: [
+      { value: 'jpg' },
+      { value: 'png' },
+      { value: 'webp' }
+    ],
+    message: 'Select a format',
+    default: ['jpg']
+  },
+  {
+    type: 'input',
+    name: 'height',
+    message: 'Maximum photo height',
+    default: 800
+  },
+  {
+    type: 'confirm',
+    name: 'keepDir',
+    message: 'keep folders',
+    default: true
+  }
+]).then(answers => {
+  converter(answers)
+})
